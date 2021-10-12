3b259302f3a3a42c4b99478c31af456372bdf397
Jace7Valentine <mitchkm12@gmail.com>
2021-10-12 13:24:53 -0700

    adds semicolons to deploy script

--- deployServer.sh
+++ deployServer.sh
@@ -2,15 +2,15 @@
 # usage deployServer.sh <path_to_server_dir>
 
 if [ -z "$1" ]; then
-    echo "No argument supplied"
-    echo "-usage: deployServer.sh <path_to_server_dir>"
+    echo "No argument supplied";
+    echo "-usage: deployServer.sh <path_to_server_dir>";
 else
-    mkdir -p $1
+    mkdir -p $1;
 
-    cp -ar client/dist $1
-    cp -a server/config/* $1
+    cp -ar client/dist $1;
+    cp -a server/config/* $1;
 
-    ln -s "$(readlink -f server/lib/server.js)" $1/server.js
+    ln -s "$(readlink -f server/lib/server.js)" $1/server.js;
 fi
 
 
